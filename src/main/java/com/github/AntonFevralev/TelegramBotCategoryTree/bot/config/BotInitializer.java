7f9f7d23225810b778206494c456d1c5ec63d017
anton <afevralevtotyt@gmail.com>
2023-10-10 15:26:03 +0200

    Добавил вывод StackTrace в BotInitializer

--- src/main/java/com/github/AntonFevralev/TelegramBotCategoryTree/bot/config/BotInitializer.java
+++ src/main/java/com/github/AntonFevralev/TelegramBotCategoryTree/bot/config/BotInitializer.java
@@ -32,7 +32,7 @@ public class BotInitializer {
         try {
             telegramBotsApi.registerBot(telegramBot);
         } catch (TelegramApiException e) {
-
+            e.printStackTrace();
         }
     }
 }
